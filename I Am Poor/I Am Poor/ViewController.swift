bcf2c98012e6792dfe5eee54afba441d7b441c00
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2020-06-22 21:38:01 +0100

    Added "I am Poor"  and Dice app
    Added "I am Poor"  and Dice app

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Bailey Search on 22/06/2020.
+//  Copyright © 2020 Bailey Search. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+print("my name is sir michael caine")
+
+let myConstant = 10
+var myVariable = 15
+
+print(myConstant + myVariable)
+
+var numbers = [45, 73, 195, 53]
+
+print(numbers[0] * numbers[1])
+print(numbers[1] * numbers[2])
+print(numbers[2] * numbers[3])

143725c166c9d85fde19a64ea5158387ae4ccc54
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2020-06-22 16:21:21 +0100

    added i am rich
    added i am rich

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Bailey Search on 21/06/2020.
+//  Copyright © 2020 Bailey Search. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
